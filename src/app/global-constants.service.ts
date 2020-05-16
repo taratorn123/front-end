df5e9c05f86ec9d290d2e8c165e06c51d506c20a
aeingza1 <38566488+aeingza1@users.noreply.github.com>
2020-05-16 12:03:41 +0700

    Fix minor issue
    - Add ฿ to campaign transaction history and convert to baht
    - Signup Form change change disabled button condition
    - add ngIf to identity verfication(Admin)

--- src/app/global-constants.service.ts
+++ src/app/global-constants.service.ts
@@ -5,7 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class GlobalConstantsService 
 {
-  public static readonly apiURL : String = 'http://34.87.165.176:8080/';
+  // public static readonly apiURL : String = 'http://34.87.165.176:8080/';
+  public static readonly apiURL : String = 'http://localhost:8080/';
+
   constructor() 
   { 
   }
